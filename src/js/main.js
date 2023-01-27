270c3291154022810d7e0094749a333a3f560bba
ar-santos <arthursantos4367@gmail.com>
2023-01-27 17:30:54 -0300

    Produtos da loja

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,18 @@
 import funcoesBanner from "./banner.js"
 import avaliacaoClientes from "./star.js"
-import abaCompraCelular from "./purchase.js"
+import abasComprar from "./purchase.js"
 
 let btnCarroseLeft = document.getElementById('btn-carrosel-left')
 let btnCarroselRight = document.getElementById('btn-carrosel-right')
+
 let containerCelular = document.getElementById('container-celular')
+let containerTenis = document.getElementById('container-tenis')
+let containerFone = document.getElementById('container-fone')
+let containerNotebook = document.getElementById('container-notebook')
+let containerMouse = document.getElementById('container-mouse')
+let containerBola = document.getElementById('container-bola')
+let containerRelogio = document.getElementById('container-relogio')
+let containerCelularTwo = document.getElementById('container-celularTwo')
 
 function carroselGallery() {
     funcoesBanner.carrosel()
@@ -27,7 +35,49 @@ btnCarroselRight.addEventListener('click', executaFuncaoBtnRight)
 avaliacaoClientes()
 
 function exibirAbaCompraCelular() {
-    abaCompraCelular()
+    abasComprar.abaCompraCelular()
 }
 
 containerCelular.addEventListener('click', exibirAbaCompraCelular)
+
+function exibirAbaCompraTenis() {
+    abasComprar.abaCompraTenis()
+}
+
+containerTenis.addEventListener('click', exibirAbaCompraTenis)
+
+function exibirAbaCompraFone() {
+    abasComprar.abaCompraFone()
+}
+
+containerFone.addEventListener('click', exibirAbaCompraFone)
+
+function exibirAbaCompraNotebook() {
+    abasComprar.abaCompraNotebook()
+}
+
+containerNotebook.addEventListener('click', exibirAbaCompraNotebook)
+
+function exibirAbaCompraMouse() {
+    abasComprar.abaCompraMouse()
+}
+
+containerMouse.addEventListener('click', exibirAbaCompraMouse)
+
+function exibirAbaCompraBola() {
+    abasComprar.abaCompraBola()
+}
+
+containerBola.addEventListener('click', exibirAbaCompraBola)
+
+function exibirAbaCompraRelogio() {
+    abasComprar.abaCompraRelogio()
+}
+
+containerRelogio.addEventListener('click', exibirAbaCompraRelogio)
+
+function exibirAbaCompraTwo() {
+    abasComprar.abaCompraCelularTwo()
+}
+
+containerCelularTwo.addEventListener('click', exibirAbaCompraTwo)
--- src/js/purchase.js
+++ src/js/purchase.js
@@ -1,17 +1,203 @@
-let containerCelular = document.getElementById('container-celular')
 let siteApp = document.getElementById('esconder-app')
-let containerCompra = document.getElementById('compra-produto')
+let compraProduto = document.getElementById('compra-produto')
+
+let containerCelular = document.getElementById('container-celular')
+let containerCompraCelular = document.getElementById('compra-produto-celular')
+
+let containerTenis = document.getElementById('container-tenis')
+let containerCompraTenis = document.getElementById('compra-produto-tenis')
+
+let containerFone = document.getElementById('container-fone')
+let containerCompraFone = document.getElementById('compra-produto-fone')
+
+let containerNotebook = document.getElementById('container-notebook')
+let containerCompraNotebook = document.getElementById('compra-produto-notebook')
+
+let containerMouse = document.getElementById('container-mouse')
+let containerCompraMouse = document.getElementById('compra-produto-mouse')
+
+let containerBola = document.getElementById('container-bola')
+let containerCompraBola = document.getElementById('compra-produto-bola')
+
+let containerRelogio = document.getElementById('container-relogio')
+let containerCompraRelogio = document.getElementById('compra-produto-relogio')
+
+let containerCelularTwo = document.getElementById('container-celularTwo')
+let containerCompraCelularTwo = document.getElementById('compra-produto-celularTwo')
 
 function abaCompraCelular() {
     if (containerCelular.style.display == 'block') {
         siteApp.style.display = 'block';
         containerCelular.style.display = 'block';
-        containerCompra.style.display = 'none';
+        containerCompraCelular.style.display = 'none';
+        compraProduto.style.display = 'none';
     }else{
         siteApp.style.display = 'none';
         containerCelular.style.display = 'none';
-        containerCompra.style.display = 'block';
+        containerCompraTenis.style.display = 'none';
+        containerCompraFone.style.display = 'none';
+        containerCompraNotebook.style.display = 'none';
+        containerCompraMouse.style.display = 'none';
+        containerCompraBola.style.display = 'none';
+        containerCompraRelogio.style.display = 'none';
+        containerCompraCelularTwo.style.display = 'none';
+        containerCompraCelular.style.display = 'block';
+        compraProduto.style.display = 'block';
     }
 }
 
-export default abaCompraCelular
+function abaCompraTenis() {
+    if (containerTenis.style.display == 'block') {
+        siteApp.style.display = 'block';
+        containerTenis.style.display = 'block';
+        containerCompraTenis.style.display = 'none';
+        compraProduto.style.display = 'none';
+    }else{
+        siteApp.style.display = 'none';
+        containerTenis.style.display = 'none';
+        containerCompraCelular.style.display = 'none';
+        containerCompraFone.style.display = 'none';
+        containerCompraNotebook.style.display = 'none';
+        containerCompraMouse.style.display = 'none';
+        containerCompraBola.style.display = 'none';
+        containerCompraRelogio.style.display = 'none';
+        containerCompraCelularTwo.style.display = 'none';
+        containerCompraTenis.style.display = 'block';
+        compraProduto.style.display = 'block';
+    }
+}
+
+function abaCompraFone() {
+    if (containerFone.style.display == 'block') {
+        siteApp.style.display = 'block';
+        containerFone.style.display = 'block';
+        compraProduto.style.display = 'none';
+    }else{
+        siteApp.style.display = 'none';
+        containerFone.style.display = 'none';
+        containerCompraCelular.style.display = 'none';
+        containerCompraTenis.style.display = 'none';
+        containerCompraNotebook.style.display = 'none';
+        containerCompraMouse.style.display = 'none';
+        containerCompraBola.style.display = 'none';
+        containerCompraRelogio.style.display = 'none';
+        containerCompraCelularTwo.style.display = 'none';
+        containerCompraFone.style.display = 'block';
+        compraProduto.style.display = 'block';
+    }
+}
+
+function abaCompraNotebook() {
+    if (containerNotebook.style.display == 'block') {
+        siteApp.style.display = 'block';
+        containerNotebok.style.display = 'block';
+        containerCompraTenis.style.display = 'none';
+        compraProduto.style.display = 'none';
+    }else{
+        siteApp.style.display = 'none';
+        containerFone.style.display = 'none';
+        containerCompraCelular.style.display = 'none';
+        containerCompraTenis.style.display = 'none';
+        containerCompraFone.style.display = 'none';
+        containerCompraMouse.style.display = 'none';
+        containerCompraBola.style.display = 'none';
+        containerCompraRelogio.style.display = 'none';
+        containerCompraCelularTwo.style.display = 'none';
+        containerCompraNotebook.style.display = 'block';
+        compraProduto.style.display = 'block';
+    }
+}
+
+function abaCompraMouse() {
+    if (containerMouse.style.display == 'block') {
+        siteApp.style.display = 'block';
+        containerMouse.style.display = 'block';
+        containerCompraTenis.style.display = 'none';
+        compraProduto.style.display = 'none';
+    }else{
+        siteApp.style.display = 'none';
+        containerFone.style.display = 'none';
+        containerCompraCelular.style.display = 'none';
+        containerCompraTenis.style.display = 'none';
+        containerCompraFone.style.display = 'none';
+        containerCompraNotebook.style.display = 'none';
+        containerCompraBola.style.display = 'none';
+        containerCompraRelogio.style.display = 'none';
+        containerCompraCelularTwo.style.display = 'none';
+        containerCompraMouse.style.display = 'block';
+        compraProduto.style.display = 'block';
+    }
+}
+
+function abaCompraBola() {
+    if (containerBola.style.display == 'block') {
+        siteApp.style.display = 'block';
+        containerBola.style.display = 'block';
+        compraProduto.style.display = 'none';
+    }else{
+        siteApp.style.display = 'none';
+        containerFone.style.display = 'none';
+        containerCompraCelular.style.display = 'none';
+        containerCompraTenis.style.display = 'none';
+        containerCompraFone.style.display = 'none';
+        containerCompraNotebook.style.display = 'none';
+        containerCompraMouse.style.display = 'none';
+        containerCompraRelogio.style.display = 'none';
+        containerCompraCelularTwo.style.display = 'none';
+        containerCompraBola.style.display = 'block';
+        compraProduto.style.display = 'block';
+    }
+}
+
+function abaCompraRelogio() {
+    if (containerRelogio.style.display == 'block') {
+        siteApp.style.display = 'block';
+        containerRelogio.style.display = 'block';
+        compraProduto.style.display = 'none';
+    }else{
+        siteApp.style.display = 'none';
+        containerFone.style.display = 'none';
+        containerCompraCelular.style.display = 'none';
+        containerCompraTenis.style.display = 'none';
+        containerCompraFone.style.display = 'none';
+        containerCompraNotebook.style.display = 'none';
+        containerCompraMouse.style.display = 'none';
+        containerCompraBola.style.display = 'none';
+        containerCompraCelularTwo.style.display = 'none';
+        containerCompraRelogio.style.display = 'block';
+        compraProduto.style.display = 'block';
+    }
+}
+
+function abaCompraCelularTwo() {
+    if (containerCelularTwo.style.display == 'block') {
+        siteApp.style.display = 'block';
+        containerCelularTwo.style.display = 'block';
+        compraProduto.style.display = 'none';
+    }else{
+        siteApp.style.display = 'none';
+        containerFone.style.display = 'none';
+        containerCompraCelular.style.display = 'none';
+        containerCompraTenis.style.display = 'none';
+        containerCompraFone.style.display = 'none';
+        containerCompraNotebook.style.display = 'none';
+        containerCompraMouse.style.display = 'none';
+        containerCompraBola.style.display = 'none';
+        containerCompraRelogio.style.display = 'none';
+        containerCompraCelularTwo.style.display = 'block';
+        compraProduto.style.display = 'block';
+    }
+}
+
+const abasComprar = {
+    abaCompraCelular,
+    abaCompraTenis,
+    abaCompraFone,
+    abaCompraNotebook,
+    abaCompraMouse,
+    abaCompraBola,
+    abaCompraRelogio,
+    abaCompraCelularTwo,
+}
+
+export default abasComprar
