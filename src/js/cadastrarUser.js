a5d55a17d9dceee398afe9d18294eb995abc3157
ar-santos <arthursantos4367@gmail.com>
2023-02-23 18:23:08 -0300

    logando usuario

--- src/js/cadastrarUser.js
+++ src/js/cadastrarUser.js
@@ -9,20 +9,26 @@ let btnCadastrar = document.getElementById("btn-cadastrar")
 let formLogin = document.getElementById("form-login")
 let formCadastrar = document.getElementById("form-cadastrar")
 
+let inputLoginNome = document.getElementById("input-login-nome")
+let inputLoginSenha = document.getElementById("input-login-senha")
+let campoObrigatorioLogin = document.getElementById("campo-obrigatorio-login")
+let campoUsuarioLogado = document.getElementById("campo-usuario-logado")
+let btnEntrar = document.getElementById("btn-entrar")
 class Usuario {
     constructor() {
         this.CadastrarUsuario();
+        this.acessarUsuario();
     }
 
     CadastrarUsuario() {
         btnCadastrar.addEventListener("click", () => {
-            if(inputCadastrarNomeUser.value && inputCadastrarSobrenomeUser.value != "") {
+            if(inputCadastrarNomeUser.value && inputCadastrarSobrenomeUser.value !="") {
                 let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
 
                 listaUser.push(
                     {
                         nomeCad: inputCadastrarNomeUser.value,
-                        usuarioCad: inputCadastrarSobrenomeUser.value,
+                        sobrenomeCad: inputCadastrarSobrenomeUser.value,
                         senhaCad: inputCadastrarSenhaUser.value
                     }
                 )
@@ -45,6 +51,59 @@ class Usuario {
                 }
             }
         });
+    }
+
+    acessarUsuario() {
+        btnEntrar.addEventListener("click", () => {
+            let listaUsuarioCadastrado = []
+
+            let userValid = {
+                nome: '',
+                sobrenome: '',
+                senha: '',
+            }
+
+            listaUsuarioCadastrado = JSON.parse(localStorage.getItem('listaUser'))
+
+            listaUsuarioCadastrado.forEach((item) => {
+                if(inputLoginNome.value == item.nomeCad && inputLoginSenha.value == item.senhaCad){
+
+                    userValid = {
+                        nome: item.nomeCad,
+                        sobrenome: item.sobrenomeCad,
+                        senha: item.senhaCad
+                    }
+                }
+            })
+
+            if(inputLoginNome.value == userValid.nome && inputLoginSenha.value == userValid.senha){
+
+                if (campoUsuarioLogado.style.display = 'none') {
+                    campoUsuarioLogado.style.display = 'flex';
+                }
+                
+                if (campoObrigatorioLogin.style.display = 'flex') {
+                    campoObrigatorioLogin.style.display = 'none';
+                }
+
+                inputLoginNome.value = ''
+                inputLoginSenha.value = ''
+                
+            }else {
+
+                if (campoObrigatorioLogin.style.display = 'none') {
+                    campoObrigatorioLogin.style.display = 'flex';
+                }
+
+                if (campoUsuarioLogado.style.display = 'flex') {
+                    campoUsuarioLogado.style.display = 'none';
+                }
+
+                inputLoginNome.value = ''
+                inputLoginSenha.value = ''
+
+            }
+        })
     }
 
 }
