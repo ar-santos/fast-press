c36247905b71dac9aa380f0e6da0d139800f2708
ar-santos <arthursantos4367@gmail.com>
2023-03-19 11:18:29 -0300

    estrutura de dados

---
+++ src/data/product_data.js
@@ -0,0 +1,30 @@
+
+let product = [
+    {"celulares":[
+        {
+            "descriptio":"Apple iPhone 14 (256 GB) – Cinza-Escuro",
+            "memory": "64GB",
+            "price": 4999.00,
+            "Promotion": 3249.00,
+            "stock": 5,
+            "rating":4.5
+        },
+        {},
+        {}]
+    },
+    {"notebooks":[
+        {
+            "descriptio":"FXiaomi Mi Notebook 14, i5-10210U MX250",
+            "ram_memory": "RAM 8 GB, DDR4-2666",
+            "disk": "SSD 250GB",
+            "price": 5999.00,
+            "Promotion": 3249.00,
+            "stock": 5,
+            "rating":4.5
+        },
+        {},
+        {}]
+    },
+    {},
+    {},
+]
---
+++ src/data/shopping_car_product.js
@@ -0,0 +1,23 @@
+
+let cartProduct =[
+    {
+        "userCarProducts":[
+        {"useId": "nomeDoUsuario",
+        "product":[
+            {
+                "produto":"produto1",
+                "quantidade":1,
+                "price": 499.90
+            }]            
+        }],
+        "userCarProducts":[
+        {"useId": "nomeDoUsuario",
+        "product":[
+            {
+                "produto":"produto1",
+                "quantidade":1,
+                "price": 499.90
+            }]            
+        }],
+    },
+]
